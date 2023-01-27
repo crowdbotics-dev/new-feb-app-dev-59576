7bb1b94503a11d0d4fd3123066afb76f51d0d759
Ravinder sales <ravinder.y@crowdbotics.com>
2023-01-27 21:15:42 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled4/index.js
@@ -0,0 +1,30 @@
+import { Pressable } from "react-native";
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled4 = ({
+  navigation
+}) => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Pressable onPress={() => navigation.navigation.navigate("undefined", {})}><View style={styles.jJgiuVwy}></View></Pressable></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  jJgiuVwy: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
+export default Untitled4;
